e14b67a4348acc146478a2578719f259d9b9f917
Chris7ianp <christian.paulo.rj@gmail.com>
2024-07-30 19:40:24 -0300

    iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+
+app.MapPost("/login", (minimal_api.DTOs.LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@test.com" && loginDTO.Senha == "123456"){
+        return Results.Ok("Login feito com sucesso");
+    }
+    else
+        return Results.Unauthorized();
+    });
+
+app.Run();
+
+
