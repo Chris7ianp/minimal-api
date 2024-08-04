e6a89ea7cc9fb825378923b85b475a7e3f826e52
Chris7ianp <christian.paulo.rj@gmail.com>
2024-08-04 15:32:21 -0300

    "Incluindo a Interface dos Veiculos"

---
+++ Dominio/Interfaces/IVeiculosSevicos.cs
@@ -0,0 +1,19 @@
+﻿using minimal_api.Dominio.Entidades;
+using minimal_api.DTOs;
+
+
+
+namespace minimal.Dominio.Interfaces;
+
+public interface IVeiculosServico
+{
+    List<Veiculo> Todos(int pagina = 1, string? nome = null, string? marca = null);
+    
+    Veiculo? BuscaPorId(int id);
+
+    void Incluir(Veiculo veiculo);
+
+    void Atualizar(Veiculo veiculo);
+
+    void Apagar(Veiculo veiculo);
+}
---
+++ Dominio/Servicos/VeiculoServico.cs
@@ -0,0 +1,60 @@
+
+using Microsoft.EntityFrameworkCore;
+using minimal.Dominio.Interfaces;
+using minimal_api.Dominio.Entidades;
+using minimal_api.DTOs;
+using minimal_api.Infraestrutura.Db;
+
+namespace minimal.Dominio.Servicos;
+
+public class VeiculoServico : IVeiculosServico
+{
+    private readonly DbContexto _contexto;
+
+    public VeiculoServico(DbContexto contexto)
+    {
+        _contexto = contexto;
+    }
+
+    public void Apagar(Veiculo veiculo)
+    {
+        _contexto.Veiculos.Remove(veiculo);
+        _contexto.SaveChanges();
+    }
+
+    public void Atualizar(Veiculo veiculo)
+    {
+        _contexto.Veiculos.Update(veiculo);
+        _contexto.SaveChanges();
+    }
+
+    public void Incluir(Veiculo veiculo)
+    {
+        _contexto.Veiculos.Add(veiculo);
+        _contexto.SaveChanges();
+    }
+
+    public Veiculo? BuscaPorId(int id)
+    {
+        return _contexto.Veiculos.Where(w => w.Id == id).FirstOrDefault();
+    }
+    
+    public List<Veiculo> Todos(int pagina = 1, string? nome = null, string? marca = null)
+    {
+        var query =_contexto.Veiculos.AsQueryable();
+        
+        if(!string.IsNullOrEmpty(nome))
+        {
+            query = query.Where(w => EF.Functions.Like(w.Nome.ToLower(), $"%{nome.ToLower()}%"));
+        }
+
+
+        int itensPorPagina = 10;
+
+        query = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
+
+        return query.ToList();
+    }
+
+    
+}
